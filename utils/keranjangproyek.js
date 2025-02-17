ba8b067e8d28bc64e5406ba683b62b70b4074eaa
Falconed11 <almacesardavid@gmail.com>
2025-02-17 16:30:10 +0700

    penawaran

--- utils/keranjangproyek.js
+++ utils/keranjangproyek.js
@@ -23,7 +23,7 @@ const table = "keranjangproyek";
 //   });
 // };
 const list = ({ id_proyek, instalasi, versi }) => {
-  const sql = `Select sp.id id_subproyek, sp.nama subproyek, kpr.nama kategoriproduk, kp.id id_keranjangproyek, kp.jumlah, kp.harga, kp.hargakustom, m.nama nmerek, v.nama nvendor, p.nama, p.stok, p.tipe, p.hargamodal, p.satuan From ${table} kp left join subproyek sp on kp.id_subproyek = sp.id left join produk p on kp.id_produk = p.id left join kategoriproduk kpr on p.id_kategori=kpr.id left join merek m on p.id_merek=m.id left join vendor v on p.id_vendor=v.id where 1=1 ${
+  const sql = `Select sp.id id_subproyek, sp.nama subproyek, kpr.nama kategoriproduk, kp.id id_keranjangproyek, kp.jumlah, kp.harga, kp.hargakustom, kp.keterangan, m.nama nmerek, v.nama nvendor, p.nama, p.stok, p.tipe, p.hargamodal, p.satuan From ${table} kp left join subproyek sp on kp.id_subproyek = sp.id left join produk p on kp.id_produk = p.id left join kategoriproduk kpr on p.id_kategori=kpr.id left join merek m on p.id_merek=m.id left join vendor v on p.id_vendor=v.id where 1=1 ${
     id_proyek ? `and kp.id_proyek=?` : ""
   } ${instalasi ? `and instalasi = ?` : ""} and versi=?`;
   const values = [];
@@ -72,6 +72,7 @@ const create = ({
   harga,
   hargakustom,
   instalasi,
+  keterangan,
   versi,
 }) => {
   if (!id_produk.length)
@@ -82,7 +83,7 @@ const create = ({
     return new Promise((resolve, reject) =>
       reject({ message: `Jumlah Belum Diisi` })
     );
-  const sql = `insert into ${table} (id_produk, id_proyek, id_subproyek, jumlah, harga, hargakustom, instalasi, versi) values (?, ?, ?, ?, ?, ?, ?, ?)`;
+  const sql = `insert into ${table} (id_produk, id_proyek, id_subproyek, jumlah, harga, hargakustom, instalasi, keterangan, versi) values (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
   const values = [
     id_produk,
     id_proyek,
@@ -91,6 +92,7 @@ const create = ({
     harga ?? 0,
     hargakustom,
     instalasi ?? 0,
+    keterangan ?? "",
     versi,
   ];
   return new Promise((resolve, reject) => {
@@ -127,14 +129,22 @@ const createNewVersion = ({ id_proyek, versi }) => {
 //   });
 // };
 
-const update = ({ id, id_subproyek, jumlah, harga, hargakustom }) => {
+const update = ({
+  id,
+  id_subproyek,
+  jumlah,
+  harga,
+  hargakustom,
+  keterangan,
+}) => {
   console.log("Harga Kustom: " + hargakustom);
-  const sql = `update ${table} set id_subproyek=?, jumlah = ?, harga = ?, hargakustom = ? where id=?`;
+  const sql = `update ${table} set id_subproyek=?, jumlah = ?, harga = ?, hargakustom = ?, keterangan = ? where id=?`;
   const values = [
     id_subproyek,
     jumlah,
     harga,
     hargakustom == "" ? null : hargakustom,
+    keterangan,
     id,
   ];
   return new Promise((resolve, reject) => {
