d468f325df2f2fa7014da55434bb6c2a01eb3318
Falconed11 <almacesardavid@gmail.com>
2025-09-10 16:45:26 +0700

    aktivitas sales

--- utils/aktivitassales.js
+++ utils/aktivitassales.js
@@ -3,8 +3,15 @@ const { pool } = require("./db.2.0.0");
 const table = "aktivitassales";
 
 const list = async ({ id_proyek = "" }) => {
-  const sql = `select * from ${table} where 1=1${id ? " and id=?" : ""}`;
-  const values = [id ?? null];
+  const sql = `select a.*, i.nama instansi, k.nama karyawan from ${table} a
+  left join karyawan k on k.id=a.id_karyawan
+  left join proyek p on p.id=a.id_proyek
+  left join instansi i on i.id=p.id_instansi
+  where 1=1${id_proyek ? " and id_proyek=? " : ""}
+  order by a.tanggal desc
+  limit 50
+  `;
+  const values = [...(id_proyek ? [id_proyek] : [])];
   const [rows] = await pool.execute(sql, values);
   return rows;
 };
@@ -13,12 +20,13 @@ const create = async ({
   id_karyawan,
   id_proyek,
   aktivitas,
-  catatan,
-  output,
-  tindakanselanjutnya,
+  catatan = "",
+  output = "",
+  tindakanselanjutnya = "",
   tanggal,
 }) => {
-  const sql = `insert into ${table} (id_karyawan, id_proyek, aktivitas, catatan, output, tindakanselanjutnya, tanggal) values (?,?,?,?,?,?,?,?)`;
+  if (!aktivitas) throw new Error("Aktivitas wajib diisi!");
+  const sql = `insert into ${table} (id_karyawan, id_proyek, aktivitas, catatan, output, tindakanselanjutnya, tanggal) values (?,?,?,?,?,?,?)`;
   const values = [
     id_karyawan,
     id_proyek,
