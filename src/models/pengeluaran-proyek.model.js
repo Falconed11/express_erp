004e788dfeaf4d51969a0f981c7f20f29f2ef75f
Falconed11 <almacesardavid@gmail.com>
2026-02-24 16:56:51 +0700

    bulanan

--- src/models/pengeluaran-proyek.model.js
+++ src/models/pengeluaran-proyek.model.js
@@ -5,9 +5,19 @@ const table = "pengeluaranproyek";
 const PengeluaranProyekModel = {
   async find({ idProyek }) {
     const [rows] = await db.execute(
-      `SELECT pp.*, p.nama produk, p.tipe, p.satuan, m.nama merek, pm.harga hargaprodukmasuk FROM ${table} pp
+      `SELECT pp.*, p.nama produk, p.tipe, p.satuan, m.nama merek, pm.harga hargaprodukmasuk, pm.terbayar-pm.jumlah*pm.harga hutang,
+        CASE
+          WHEN pp.id_produkkeluar IS NOT NULL THEN
+            CASE
+              WHEN (pm.terbayar-(pm.jumlah*pm.harga)) = 0 THEN 1
+              ELSE 0
+            END
+          ELSE pp.status
+        END AS lunas, 
+      k.nama karyawan FROM ${table} pp
       left join produk p on p.id=pp.id_produk
       left join merek m on m.id=p.id_merek
+      left join karyawan k on k.id=pp.id_karyawan
       left join produkkeluar pk on pk.id=pp.id_produkkeluar
       left join produkmasuk pm on pm.id=pk.id_produkmasuk
       where 1=1
