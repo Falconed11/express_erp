ee935ecc95db507e2e0a1463115efbc3d78ca895
Falconed11 <almacesardavid@gmail.com>
2026-02-27 16:58:55 +0700

    modal

--- src/services/perusahaan.service.js
+++ src/services/perusahaan.service.js
@@ -1,4 +1,7 @@
 import { getAll } from "../models/default.model.js";
+import OperasionalKantorService from "./operasional-kantor.service.js";
+import PembayaranProyekService from "./pembayaran-proyek.service.js";
+import PengeluaranProyekService from "./pengeluaran-proyek.service.js";
 
 const table = "perusahaan";
 
@@ -6,5 +9,12 @@ const PerusahaanService = {
   async get() {
     return getAll(table);
   },
+  async getMonthlyReport(param) {
+    const pembayaran = await PembayaranProyekService.get(param);
+    const pengeluaran = await PengeluaranProyekService.get(param);
+    const operasional = await OperasionalKantorService.getAll(param);
+    return pembayaran - (pengeluaran + operasional);
+  },
 };
+
 export default PerusahaanService;
